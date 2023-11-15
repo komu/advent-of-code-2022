441646e122f1fccf2370bdb9a13e83e465e9de48
Juha Komulainen <juha.komulainen@evident.fi>
2023-11-15 22:35:51 +0200

    Add solution for day 23

--- README.md
+++ README.md
@@ -8,17 +8,17 @@ a total budget of 100 milliseconds.
 
 ## Results
 
-| Day                                            | Solution               | Part 1   | Part 2   | Notes |
-| :--------------------------------------------: | :--------------------- | -------: | -------: | :---- |
-| [1](https://adventofcode.com/2022/day/1)   | [01.rs](src/bin/01.rs) |  77.66µs |  69.09µs | Process everything in single iteration, avoid sorting results in part 2 |
-| [2](https://adventofcode.com/2022/day/2)   | [02.rs](src/bin/02.rs) |  81.87µs |  62.62µs | Use suitable representations to allow using modular arithmetic for comparisons |
-| [3](https://adventofcode.com/2022/day/3)   | [03.rs](src/bin/03.rs) |  54.33µs |  51.12µs | Represent rucksack as a bitset |
-| [4](https://adventofcode.com/2022/day/4)   | [04.rs](src/bin/04.rs) |  82.56µs |  69.46µs | - |
-| [5](https://adventofcode.com/2022/day/5)   | [05.rs](src/bin/05.rs) |  40.67µs |  36.89µs | - |
-| [6](https://adventofcode.com/2022/day/6)   | [06.rs](src/bin/06.rs) |   5.00µs |   7.09µs | Calculate forward skips to avoid processing most of the input |
-| [7](https://adventofcode.com/2022/day/7)   | [07.rs](src/bin/07.rs) |  43.68µs |  60.68µs | Avoid actually building the tree |
-| [8](https://adventofcode.com/2022/day/8)   | [08.rs](src/bin/08.rs) |  78.85µs | 161.86µs | Precalculate maximums for each side to speed up part 1 |
-| [9](https://adventofcode.com/2022/day/9)   | [09.rs](src/bin/09.rs) | 352.48µs | 784.74µs | -     |
+|                    Day                     | Solution               | Part 1   | Part 2   | Notes |
+|:------------------------------------------:|:-----------------------| -------: | -------: | :--- |
+|  [1](https://adventofcode.com/2022/day/1)  | [01.rs](src/bin/01.rs) |  77.66µs |  69.09µs | Process everything in single iteration, avoid sorting results in part 2 |
+|  [2](https://adventofcode.com/2022/day/2)  | [02.rs](src/bin/02.rs) |  81.87µs |  62.62µs | Use suitable representations to allow using modular arithmetic for comparisons |
+|  [3](https://adventofcode.com/2022/day/3)  | [03.rs](src/bin/03.rs) |  54.33µs |  51.12µs | Represent rucksack as a bitset |
+|  [4](https://adventofcode.com/2022/day/4)  | [04.rs](src/bin/04.rs) |  82.56µs |  69.46µs | - |
+|  [5](https://adventofcode.com/2022/day/5)  | [05.rs](src/bin/05.rs) |  40.67µs |  36.89µs | - |
+|  [6](https://adventofcode.com/2022/day/6)  | [06.rs](src/bin/06.rs) |   5.00µs |   7.09µs | Calculate forward skips to avoid processing most of the input |
+|  [7](https://adventofcode.com/2022/day/7)  | [07.rs](src/bin/07.rs) |  43.68µs |  60.68µs | Avoid actually building the tree |
+|  [8](https://adventofcode.com/2022/day/8)  | [08.rs](src/bin/08.rs) |  78.85µs | 161.86µs | Precalculate maximums for each side to speed up part 1 |
+|  [9](https://adventofcode.com/2022/day/9)  | [09.rs](src/bin/09.rs) | 352.48µs | 784.74µs | -    |
 | [10](https://adventofcode.com/2022/day/10) | [10.rs](src/bin/10.rs) |   5.37µs |   9.09µs | -     |
 | [11](https://adventofcode.com/2022/day/11) | [11.rs](src/bin/11.rs) |  25.28µs |   5.42ms | 😔    |
 | [12](https://adventofcode.com/2022/day/12) | [12.rs](src/bin/12.rs) | 767.46µs | 985.24µs | Search using A*, use custom map for distances |
@@ -32,6 +32,7 @@ a total budget of 100 milliseconds.
 | [20](https://adventofcode.com/2022/day/20) | [20.rs](src/bin/20.rs) |   6.72ms |  78.69ms | 😔 |
 | [21](https://adventofcode.com/2022/day/21) | [21.rs](src/bin/21.rs) | 325.30µs | 235.40µs | - |
 | [22](https://adventofcode.com/2022/day/22) | [22.rs](src/bin/22.rs) | 139.31µs | 127.73µs | - |
+| [23](https://adventofcode.com/2022/day/23) | [23.rs](src/bin/23.rs) |  15.51ms |  831.17ms | 😔 |
 
 (Totally unscientific numbers from a single run, will improve these in the future.)
 
--- src/bin/23.rs
+++ src/bin/23.rs
@@ -1,9 +1,213 @@
+use enum_iterator::{all, Sequence};
+use hashbrown::{HashMap, HashSet};
+use itertools::Itertools;
+
+#[derive(Sequence)]
+enum Direction {
+    N,
+    S,
+    W,
+    E,
+    NE,
+    NW,
+    SE,
+    SW,
+}
+
+impl Direction {
+    fn deltas(self) -> (i32, i32) {
+        match self {
+            Direction::N => (0, -1),
+            Direction::S => (0, 1),
+            Direction::W => (-1, 0),
+            Direction::E => (1, 0),
+            Direction::NE => (1, -1),
+            Direction::NW => (-1, -1),
+            Direction::SE => (1, 1),
+            Direction::SW => (-1, 1),
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum CardinalDirection {
+    N,
+    S,
+    W,
+    E,
+}
+
+impl CardinalDirection {
+    fn deltas(self) -> (i32, i32) {
+        match self {
+            CardinalDirection::N => (0, -1),
+            CardinalDirection::S => (0, 1),
+            CardinalDirection::W => (-1, 0),
+            CardinalDirection::E => (1, 0),
+        }
+    }
+
+    fn adjacent(self) -> Vec<Direction> {
+        match self {
+            CardinalDirection::N => vec![Direction::N, Direction::NE, Direction::NW],
+            CardinalDirection::S => vec![Direction::S, Direction::SE, Direction::SW],
+            CardinalDirection::W => vec![Direction::W, Direction::NW, Direction::SW],
+            CardinalDirection::E => vec![Direction::E, Direction::NE, Direction::SE],
+        }
+    }
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    Some(run(input, 10, false))
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(run(input, 1_000_000, true))
+}
+
+pub fn run(input: &str, rounds: u32, round_count: bool) -> u32 {
+    let mut elves = ElfMap::parse(input);
+
+    let directions_vecs = vec![
+        vec![CardinalDirection::N, CardinalDirection::S, CardinalDirection::W, CardinalDirection::E],
+        vec![CardinalDirection::S, CardinalDirection::W, CardinalDirection::E, CardinalDirection::N],
+        vec![CardinalDirection::W, CardinalDirection::E, CardinalDirection::N, CardinalDirection::S],
+        vec![CardinalDirection::E, CardinalDirection::N, CardinalDirection::S, CardinalDirection::W],
+    ];
+
+    for r in 0..rounds {
+        let directions = &directions_vecs[r as usize % 4];
+
+        let mut proposals_counts = HashMap::<Point, usize>::new();
+
+        // collect proposals
+        for elf in &elves.elves {
+            if let Some(proposal) = elves.proposal(elf, directions) {
+                *proposals_counts.entry(proposal).or_insert(0) += 1;
+            }
+        }
+
+        let mut next_gen = ElfMap::new();
+
+        // act on proposals
+        let mut moves = 0;
+        for elf in &elves.elves {
+            let new_pos = if let Some(proposal) = elves.proposal(elf, directions) {
+                if let Some(1) = proposals_counts.get(&proposal) {
+                    moves += 1;
+                    proposal
+                } else {
+                    *elf
+                }
+            } else {
+                *elf
+            };
+            next_gen.elves.insert(new_pos);
+        }
+
+        if moves == 0 && round_count {
+            return r + 1;
+        }
+        elves = next_gen;
+    }
+
+    if round_count {
+        panic!("no result");
+    }
+
+    let (x_min, x_max) = elves.elves.iter().map(|e| e.x).minmax().into_option().unwrap();
+    let (y_min, y_max) = elves.elves.iter().map(|e| e.y).minmax().into_option().unwrap();
+
+    let w = (x_max - x_min + 1) as u32;
+    let h = (y_max - y_min + 1) as u32;
+    w * h - elves.elves.len() as u32
+}
+
+type Point = aoc::point::Point<i32>;
+
+#[derive(Debug)]
+struct ElfMap {
+    elves: HashSet<Point>,
+}
+
+impl ElfMap {
+    fn proposal(&self, elf: &Point, directions: &[CardinalDirection]) -> Option<Point> {
+        if self.is_alone(elf) {
+            return None
+        }
+        for &cd in directions {
+            if self.is_free(elf, cd) {
+                let (dx, dy) = cd.deltas();
+                return Some(elf.towards(dx, dy))
+            }
+        }
+        None
+    }
+}
+
+impl ElfMap {
+    fn is_alone(&self, p: &Point) -> bool {
+        for d in all::<Direction>() {
+            let (dx, dy) = d.deltas();
+            let p2 = p.towards(dx, dy);
+            if self.elves.contains(&p2) {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn is_free(&self, p: &Point, cd: CardinalDirection) -> bool {
+        for d in cd.adjacent() {
+            let (dx, dy) = d.deltas();
+            let p2 = p.towards(dx, dy);
+            if self.elves.contains(&p2) {
+                return false;
+            }
+        }
+        true
+    }
+
+    #[allow(dead_code)]
+    fn dump(&self) {
+        let (x_min, x_max) = self.elves.iter().map(|e| e.x).minmax().into_option().unwrap();
+        let (y_min, y_max) = self.elves.iter().map(|e| e.y).minmax().into_option().unwrap();
+
+        for y in y_min..=y_max {
+            for x in x_min..=x_max {
+                let p = Point { x, y };
+                if self.elves.contains(&p) {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+            }
+            println!()
+        }
+        println!();
+    }
+}
+
+impl ElfMap {
+    fn new() -> Self {
+        ElfMap { elves: HashSet::new() }
+    }
+
+    fn parse(s: &str) -> Self {
+        let mut elves = HashSet::new();
+
+        for (y, line) in s.lines().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                if c == '#' {
+                    elves.insert(Point { x: x as i32, y: y as i32 });
+                }
+            }
+        }
+
+        ElfMap {
+            elves
+        }
+    }
 }
 
 fn main() {
@@ -19,12 +223,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = aoc::read_file("examples", 23);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(110));
     }
 
     #[test]
     fn test_part_two() {
         let input = aoc::read_file("examples", 23);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(20));
     }
 }
