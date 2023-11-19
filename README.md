bfb956c97f70a26af952f877eb87c2da64f43df7
Juha Komulainen <juha.komulainen@evident.fi>
2023-11-19 17:20:06 +0200

    Optimization

--- README.md
+++ README.md
@@ -8,9 +8,8 @@ a total budget of 100 milliseconds.
 
 ## Results
 
-All rows with solutions over a millisecond are marked with 😔, those with over 50 ms are marked with 😔😔 and those
-with over 1 second with 😔😔😔.
-
+All rows with solutions over a millisecond are marked with 😔, those with over 50 ms are marked with 😔😔. The days
+exceeding 500 ms are marked with 😔😔😔.
 
 |                    Day                     | Solution               |   Part 1 |   Part 2 | Notes                                                                                                                        |
 |:------------------------------------------:|:-----------------------|---------:|---------:|:-----------------------------------------------------------------------------------------------------------------------------|
@@ -32,16 +31,18 @@ with over 1 second with 😔😔😔.
 | [16](https://adventofcode.com/2022/day/16) | [16.rs](src/bin/16.rs) |   9.92ms |   6.53ms | 😔                                                                                                                           |
 | [17](https://adventofcode.com/2022/day/17) | [17.rs](src/bin/17.rs) | 221.94µs | 326.42µs | Keep previous states in cache to find the period of the process                                                              |
 | [18](https://adventofcode.com/2022/day/18) | [18.rs](src/bin/18.rs) | 160.66µs | 437.68µs | Represent the set of points as a bit-set                                                                                     |
-| [19](https://adventofcode.com/2022/day/19) | [19.rs](src/bin/19.rs) | 591.35ms |    1.11s | 😔😔😔 Heuristics to direct the search, memoization, pruning against best result seen so far                                 |
+| [19](https://adventofcode.com/2022/day/19) | [19.rs](src/bin/19.rs) | 582.48ms |    1.06s | 😔😔😔 Heuristics to direct the search, memoization, pruning against best result seen so far                                 |
 | [20](https://adventofcode.com/2022/day/20) | [20.rs](src/bin/20.rs) |   5.08ms |  53.33ms | 😔😔                                                                                                                         |
 | [21](https://adventofcode.com/2022/day/21) | [21.rs](src/bin/21.rs) | 325.30µs | 235.40µs | -                                                                                                                            |
 | [22](https://adventofcode.com/2022/day/22) | [22.rs](src/bin/22.rs) | 139.31µs | 127.73µs | -                                                                                                                            |
 | [23](https://adventofcode.com/2022/day/23) | [23.rs](src/bin/23.rs) | 916.03µs |  87.09ms | 😔😔 Keep three different data structures to make everything inside the loop O(1). Use suitable bit-representation for data. |
-| [24](https://adventofcode.com/2022/day/24) | [24.rs](src/bin/24.rs) |  80.10ms |    1.16s | 😔😔😔                                                                                                                       |
+| [24](https://adventofcode.com/2022/day/24) | [24.rs](src/bin/24.rs) |  14.71ms |  26.31ms | 😔Search using A* with a smart heuristic. Represent each state in 32 bits. Use Vector instead of Map for g-scores.           |
 | [25](https://adventofcode.com/2022/day/25) | [25.rs](src/bin/25.rs) |  17.04µs |        - | -                                                                                                                            |
 
-In the end, days 15, 19 and 24 blew the 100 ms budget by themselves while days 20 and 23 were over 50 ms.
-Ignoring those outliers, the total time for the rest of the 20 days is 28 ms, which is pretty decent.
+In the end, days 15 and 19 blew the 100 ms budget by themselves while days 20 and 23 were over 50 ms.
+Ignoring those outliers, the total time for the rest of the 21 days is 69 ms, which is pretty decent, especially
+considering that only day 19 exceeded total runtime of one second and even that is under two seconds. That said,
+the total runtime for all days is 1.98 seconds, which almost 20 times more than the budgeted 100 ms.
 
 ## Previous years
 
--- src/bin/24.rs
+++ src/bin/24.rs
@@ -1,28 +1,72 @@
-use aoc::point::CardinalDirection;
-use aoc::shortest_path::{shortest_path_len, Graph};
-use hashbrown::HashMap;
+use std::cmp::Reverse;
 use std::iter::once;
 
-pub fn part_one(input: &str) -> Option<u32> {
+use hashbrown::HashMap;
+use priority_queue::PriorityQueue;
+
+use TripState::{Initial, VisitedEnd, VisitedStartAfterEnd};
+
+pub fn part_one(input: &str) -> Option<u16> {
     Basin::parse(input, false).shortest_path()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u16> {
     Basin::parse(input, true).shortest_path()
 }
 
-type Minutes = u16;
+type Minutes = i16;
 type Coordinate = i8;
 
 const DIRECTIONS: [(Coordinate, Coordinate); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
 
 type Point = aoc::point::Point<Coordinate>;
 
-#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone)]
+#[derive(Debug, Eq, PartialEq, Hash)]
 struct SearchState {
     pos: Point,
-    minutes: Minutes,
-    state: TripState,
+
+    // bottom 2 bits is TripState, rest is minutes
+    minutes_and_trip_state: u16,
+}
+
+const MAX_WIDTH: usize = 128;
+const MAX_HEIGHT: usize = 32;
+const MAX_MINUTES: usize = 1024;
+const MAX_TRIP_STATE: usize = 3;
+const TRIP_STATE_BITS: u8 = 2;
+
+impl SearchState {
+    fn new(pos: Point, minutes: Minutes, state: TripState) -> Self {
+        SearchState {
+            pos,
+            minutes_and_trip_state: (minutes as u16) << TRIP_STATE_BITS | (state as u16),
+        }
+    }
+
+    fn index(&self) -> usize {
+        let x = (self.pos.x + 1) as usize;
+        let y = (self.pos.y + 1) as usize;
+        (x * MAX_HEIGHT + y) * MAX_MINUTES + (self.minutes_and_trip_state as usize)
+    }
+
+    fn wait(&self) -> SearchState {
+        SearchState::new(self.pos, self.minutes() + 1, self.state())
+    }
+
+    #[inline]
+    fn minutes(&self) -> Minutes {
+        (self.minutes_and_trip_state >> TRIP_STATE_BITS) as Minutes
+    }
+
+    #[inline]
+    fn state(&self) -> TripState {
+        match self.minutes_and_trip_state & 3 {
+            0 => Initial,
+            1 => VisitedEnd,
+            2 => VisitedStartAfterEnd,
+            _ => panic!("invalid TripState"),
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -35,29 +79,21 @@ enum TripState {
 #[derive(Debug)]
 struct Blizzard {
     pos: Point,
-    dir: CardinalDirection,
 }
 
 impl Blizzard {
-    fn position(&self, minutes: Minutes, width: Coordinate, height: Coordinate) -> Point {
-        let mut x = self.pos.x as i32;
-        let mut y = self.pos.y as i32;
-        let w = width as i32;
-        let h = height as i32;
-        let mw = (minutes as i32) % w;
-        let mh = (minutes as i32) % h;
+    fn new(pos: Point) -> Self {
+        Blizzard { pos }
+    }
 
-        match self.dir {
-            CardinalDirection::E => x = (x + mw) % w,
-            CardinalDirection::W => x = (x + w - mw) % w,
-            CardinalDirection::S => y = (y + mh) % h,
-            CardinalDirection::N => y = (y + h - mh) % h,
-        };
+    #[inline]
+    fn x_at_offset(&self, m: i16, w: i16) -> Coordinate {
+        ((self.pos.x as i16 + m) % w) as Coordinate
+    }
 
-        Point {
-            x: x as Coordinate,
-            y: y as Coordinate,
-        }
+    #[inline]
+    fn y_at_offset(&self, m: i16, h: i16) -> Coordinate {
+        ((self.pos.y as i16 + m) % h) as Coordinate
     }
 }
 
@@ -67,8 +103,8 @@ struct Basin {
     end: Point,
     width: Coordinate,
     height: Coordinate,
-    blizzards_by_row: HashMap<Coordinate, Vec<Blizzard>>,
-    blizzards_by_col: HashMap<Coordinate, Vec<Blizzard>>,
+    blizzards_by_row: HashMap<Coordinate, (Vec<Blizzard>, Vec<Blizzard>)>,
+    blizzards_by_col: HashMap<Coordinate, (Vec<Blizzard>, Vec<Blizzard>)>,
     go_back_to_start: bool,
 }
 
@@ -76,31 +112,41 @@ impl Basin {
     fn parse(s: &str, go_back_to_start: bool) -> Self {
         let lines: Vec<_> = s.lines().filter(|l| l.starts_with('#')).collect();
 
-        let mut blizzards_by_row = HashMap::<Coordinate, Vec<Blizzard>>::new();
-        let mut blizzards_by_col = HashMap::<Coordinate, Vec<Blizzard>>::new();
+        let mut blizzards_by_row = HashMap::<Coordinate, (Vec<Blizzard>, Vec<Blizzard>)>::new();
+        let mut blizzards_by_col = HashMap::<Coordinate, (Vec<Blizzard>, Vec<Blizzard>)>::new();
 
         for (y, l) in lines.iter().skip(1).take(lines.len() - 2).enumerate() {
             let line = &l.as_bytes()[1..l.len() - 1];
 
             for (x, &c) in line.iter().enumerate() {
                 if c != b'.' {
-                    let blizzard = Blizzard {
-                        pos: Point {
-                            x: x as Coordinate,
-                            y: y as Coordinate,
-                        },
-                        dir: CardinalDirection::for_code(c as char),
+                    let pos = Point {
+                        x: x as Coordinate,
+                        y: y as Coordinate,
                     };
 
-                    match blizzard.dir {
-                        CardinalDirection::N | CardinalDirection::S => blizzards_by_col
-                            .entry(blizzard.pos.x)
-                            .or_default()
-                            .push(blizzard),
-                        CardinalDirection::W | CardinalDirection::E => blizzards_by_row
-                            .entry(blizzard.pos.y)
-                            .or_default()
-                            .push(blizzard),
+                    match c {
+                        b'v' => blizzards_by_col
+                            .entry(pos.x)
+                            .or_insert_with(|| (Vec::new(), Vec::new()))
+                            .0
+                            .push(Blizzard::new(pos)),
+                        b'^' => blizzards_by_col
+                            .entry(pos.x)
+                            .or_insert_with(|| (Vec::new(), Vec::new()))
+                            .1
+                            .push(Blizzard::new(pos)),
+                        b'>' => blizzards_by_row
+                            .entry(pos.y)
+                            .or_insert_with(|| (Vec::new(), Vec::new()))
+                            .0
+                            .push(Blizzard::new(pos)),
+                        b'<' => blizzards_by_row
+                            .entry(pos.y)
+                            .or_insert_with(|| (Vec::new(), Vec::new()))
+                            .1
+                            .push(Blizzard::new(pos)),
+                        _ => panic!("invalid direction {c}"),
                     }
                 }
             }
@@ -123,105 +169,118 @@ impl Basin {
     }
 
     fn is_empty(&self, p: Point, minutes: Minutes) -> bool {
-        if p == self.start || p == self.end {
-            true
-        } else if p.x < 0 || p.y < 0 || p.x >= self.width || p.y >= self.height {
-            false
+        if p.x < 0 || p.y < 0 || p.x >= self.width || p.y >= self.height {
+            p == self.start || p == self.end
         } else {
-            if let Some(blizzards) = self.blizzards_by_col.get(&p.x) {
-                if blizzards
-                    .iter()
-                    .any(|b| b.position(minutes, self.width, self.height) == p)
-                {
+            if let Some((forward, backward)) = self.blizzards_by_col.get(&p.x) {
+                let h = self.height as i16;
+                let m = minutes % h;
+                let m2 = h - m % h;
+                if forward.iter().any(|b| b.y_at_offset(m, h) == p.y) {
+                    return false;
+                }
+                if backward.iter().any(|b| b.y_at_offset(m2, h) == p.y) {
                     return false;
                 }
             }
-            if let Some(blizzards) = self.blizzards_by_row.get(&p.y) {
-                if blizzards
-                    .iter()
-                    .any(|b| b.position(minutes, self.width, self.height) == p)
-                {
+            if let Some((forward, backward)) = self.blizzards_by_row.get(&p.y) {
+                let w = self.width as i16;
+                let m = minutes % w;
+                let m2 = w - m % w;
+                if forward.iter().any(|b| b.x_at_offset(m, w) == p.x) {
+                    return false;
+                }
+                if backward.iter().any(|b| b.x_at_offset(m2, w) == p.x) {
                     return false;
                 }
             }
-            return true;
+
+            true
         }
     }
 
-    fn shortest_path(&self) -> Option<u32> {
-        let start = SearchState {
-            pos: self.start,
-            minutes: 0,
-            state: TripState::Initial,
-        };
-        shortest_path_len(self, start).map(|x| x.1)
+    fn shortest_path(&self) -> Option<u16> {
+        let start = SearchState::new(self.start, 0, Initial);
+
+        let mut g_score = vec![u16::MAX; MAX_HEIGHT * MAX_WIDTH * MAX_MINUTES * MAX_TRIP_STATE];
+        let mut open_set = PriorityQueue::<SearchState, Reverse<u16>>::new();
+
+        g_score[start.index()] = 0;
+        let start_distance = self.heuristic_distance(&start);
+        open_set.push(start, Reverse(start_distance));
+
+        while let Some((current, _)) = open_set.pop() {
+            let current_gscore = current.minutes() as u16;
+
+            if current.pos == self.end
+                && (!self.go_back_to_start || current.state() == VisitedStartAfterEnd)
+            {
+                return Some(current.minutes() as u16);
+            };
+            for neighbor in self.neighbors(&current) {
+                let tentative_gscore = current_gscore + 1;
+                if tentative_gscore < g_score[neighbor.index()] {
+                    let neighbor_score = tentative_gscore + self.heuristic_distance(&neighbor);
+
+                    g_score[neighbor.index()] = tentative_gscore;
+                    open_set.push(neighbor, Reverse(neighbor_score));
+                }
+            }
+        }
+
+        None
     }
-}
 
-impl Graph for Basin {
-    type Node = SearchState;
-
-    fn is_solution(&self, node: &Self::Node) -> bool {
-        node.pos == self.end
-            && (!self.go_back_to_start || node.state == TripState::VisitedStartAfterEnd)
-    }
-
-    fn collect_neighbors(&self, node: &Self::Node, neighbors: &mut Vec<(Self::Node, u32)>) {
+    fn neighbors(&self, node: &SearchState) -> impl Iterator<Item = SearchState> + '_ {
         let current_pos = node.pos;
-        let current_minutes = node.minutes;
-        let current_state = node.state;
+        let next_minute = node.minutes() + 1;
+        let current_state = node.state();
 
-        let wait_state = SearchState {
-            pos: current_pos,
-            minutes: current_minutes + 1,
-            state: current_state,
-        };
+        let wait_state = node.wait();
 
         let move_states = DIRECTIONS.iter().map(move |(dx, dy)| {
-            use TripState::*;
-
             let pos = current_pos.towards(*dx, *dy);
-            SearchState {
-                pos,
-                minutes: current_minutes + 1,
-                state: match current_state {
-                    Initial => {
-                        if pos == self.end {
-                            VisitedEnd
-                        } else {
-                            Initial
-                        }
+            let next_state = match current_state {
+                Initial => {
+                    if pos == self.end {
+                        VisitedEnd
+                    } else {
+                        Initial
                     }
-                    VisitedEnd => {
-                        if pos == self.start {
-                            VisitedStartAfterEnd
-                        } else {
-                            VisitedEnd
-                        }
+                }
+                VisitedEnd => {
+                    if pos == self.start {
+                        VisitedStartAfterEnd
+                    } else {
+                        VisitedEnd
                     }
-                    VisitedStartAfterEnd => VisitedStartAfterEnd,
-                },
-            }
+                }
+                VisitedStartAfterEnd => VisitedStartAfterEnd,
+            };
+
+            SearchState::new(pos, next_minute, next_state)
         });
 
-        let cs = move_states
+        move_states
             .chain(once(wait_state))
-            .filter(|s| self.is_empty(s.pos, s.minutes))
-            .map(|n| (n, 1));
-
-        neighbors.extend(cs);
+            .filter(|s| self.is_empty(s.pos, s.minutes()))
     }
 
-    fn heuristic_distance(&self, node: &Self::Node) -> u32 {
-        if self.go_back_to_start {
-            let start_to_end = self.start.manhattan_distance(&self.end);
-            (match node.state {
-                TripState::Initial => 2 * start_to_end + node.pos.manhattan_distance(&self.end),
-                TripState::VisitedEnd => start_to_end + node.pos.manhattan_distance(&self.start),
-                TripState::VisitedStartAfterEnd => node.pos.manhattan_distance(&self.end),
-            }) as u32
+    fn heuristic_distance(&self, node: &SearchState) -> u16 {
+        let h = if self.go_back_to_start {
+            let start_to_end = self.start.manhattan_distance(&self.end) as u16;
+            match node.state() {
+                Initial => 2 * start_to_end + node.pos.manhattan_distance(&self.end) as u16,
+                VisitedEnd => start_to_end + node.pos.manhattan_distance(&self.start) as u16,
+                VisitedStartAfterEnd => node.pos.manhattan_distance(&self.end) as u16,
+            }
         } else {
-            node.pos.manhattan_distance(&self.end) as u32
+            node.pos.manhattan_distance(&self.end) as u16
+        };
+        if h > 20 {
+            h * 2
+        } else {
+            h
         }
     }
 }
@@ -235,26 +294,6 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_blizzard_position_right() {
-        let w = 7;
-        let h = 5;
-        let blizzard = Blizzard {
-            pos: Point { x: 2, y: 4 },
-            dir: CardinalDirection::E,
-        };
-
-        assert_eq!(Point { x: 2, y: 4 }, blizzard.position(0, w, h));
-        assert_eq!(Point { x: 3, y: 4 }, blizzard.position(1, w, h));
-        assert_eq!(Point { x: 4, y: 4 }, blizzard.position(2, w, h));
-        assert_eq!(Point { x: 5, y: 4 }, blizzard.position(3, w, h));
-        assert_eq!(Point { x: 6, y: 4 }, blizzard.position(4, w, h));
-        assert_eq!(Point { x: 0, y: 4 }, blizzard.position(5, w, h));
-        assert_eq!(Point { x: 1, y: 4 }, blizzard.position(6, w, h));
-        assert_eq!(Point { x: 2, y: 4 }, blizzard.position(7, w, h));
-        assert_eq!(Point { x: 3, y: 4 }, blizzard.position(8, w, h));
-    }
 
     #[test]
     fn test_part_one() {
